019140cdd534ab5240371927a2960e837cb47050
smbrisard <smbrisard@gmail.com>
2026-04-26 17:29:46 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// v19g 1777246397
+// v19h 1777249748
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(c){return caches.delete(c);}));}).then(function(){return self.clients.claim();}));});
