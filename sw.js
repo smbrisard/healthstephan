0d2f111a2215ce7b397774bf131a0b48948a36be
smbrisard <smbrisard@gmail.com>
2026-05-03 12:47:27 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// FORCE-UPDATE-v21ah 1777837013
+// FORCE-UPDATE-v21ai 1777837488
 self.addEventListener('install',function(e){e.waitUntil(self.skipWaiting());});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(names){return Promise.all(names.map(function(n){return caches.delete(n);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request.clone()).catch(function(){return caches.match(e.request);}));});
