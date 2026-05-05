9b63c1daa354942da00c108be06a8ef2e4ea865e
smbrisard <smbrisard@gmail.com>
2026-05-05 06:28:26 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* v21ay 1777987007 */
-var CACHE='hs-v21ay';
+/* v21az 1777987658 */
+var CACHE='hs-v21az';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){if(e.request.method!=='GET') return;e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
