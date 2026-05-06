dad94793fc64df91380f24d573ea4744a2f21534
smbrisard <smbrisard@gmail.com>
2026-05-06 14:32:35 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* v21bl 1778081337 */
-var CACHE='hs-v21bl';
+/* v21bm 1778100320 */
+var CACHE='hs-v21bm';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){if(e.request.method!=='GET') return;e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
